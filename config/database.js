e30aaa96819dadc7f05126ae4e37032f2b7bb4c5
icarodavi <icarodavi@gmail.com>
2018-06-02 10:39:25 -0300

    hrk

--- config/database.js
+++ config/database.js
@@ -1,7 +1,14 @@
+// module.exports = {
+//   username: 'root',
+//   password: null,
+//   database: 'pay',
+//   host: '127.0.0.1',
+//   dialect: 'mysql',
+// };
 module.exports = {
-  username: 'root',
-  password: null,
-  database: 'pay',
-  host: '127.0.0.1',
-  dialect: 'mysql',
+  username: 'wqemgfosnrwzou',
+  password: '7a548d85a7b06edd7e38a8889fa4b1a81932095d5ae01c212c72ea97442683cf',
+  port: '5432',
+  host: 'ec2-54-225-96-191.compute-1.amazonaws.com',
+  dialect: 'postgres',
 };

62e1b9ae92f0918a2db69456b801deffe0a805c2
icarodavi <icarodavi@gmail.com>
2018-06-22 14:37:00 -0300

    up

--- app/models/section.js
+++ app/models/section.js
@@ -3,11 +3,9 @@ const hljs = require('highlight.js');
 const md = require('markdown-it')({
   highlight: (str, lang) => {
     if (lang && hljs.getLanguage(lang)) {
-      return (
-        `<pre class"hljs"><code>${hljs.highlight(lang, str.trim(), true).value}</code></pre>`
-      );
+      return `<pre class="hljs"><code>${hljs.highlight(lang, str.trim(), true).value}</code></pre>`;
     }
-    return `<pre class"hljs"><code>${md.utils.escapeHtml(str.trim())}</code></pre>`;
+    return `<pre class="hljs"><code>${md.utils.escapeHtml(str.trim())}</code></pre>`;
   },
 });
 
